4a50fed25b0a1ca6361eaa37da5406f891c8cc3c
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-31 19:29:52 -0500

    Jørgen Pedersen Gram an#d Erhard Schmidt
    Add string comparison and similarity methods
    
    Updated StringExtensions with new methods: DamerauLevenshteinDistance, JaccardSimilarity, JaroDistance, and JaroWinklerDistance for enhanced string comparison and similarity measurement.

--- CSharpDataStructureAndAlogrithm/Algorithm/StringExtensions.cs
+++ CSharpDataStructureAndAlogrithm/Algorithm/StringExtensions.cs
@@ -41,4 +41,109 @@ public static class StringExtensions
     {
         return input.Any(c => c > MaxAnsiCode);
     }
+
+    public static int DamerauLevenshteinDistance(this string s1, string s2)
+    {
+        int len1 = s1.Length;
+        int len2 = s2.Length;
+        int[,] d = new int[len1 + 1, len2 + 1];
+
+        for (int i = 0; i <= len1; i++) d[i, 0] = i;
+        for (int j = 0; j <= len2; j++) d[0, j] = j;
+
+        for (int i = 1; i <= len1; i++)
+        {
+            for (int j = 1; j <= len2; j++)
+            {
+                int cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+
+                d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+
+                if (i > 1 && j > 1 && s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1])
+                {
+                    d[i, j] = Math.Min(d[i, j], d[i - 2, j - 2] + cost);
+                }
+            }
+        }
+
+        return d[len1, len2];
+    }
+
+    public static double JaccardSimilarity(this string s1, string s2)
+    {
+        HashSet<char> set1 = new HashSet<char>(s1);
+        HashSet<char> set2 = new HashSet<char>(s2);
+
+        HashSet<char>? intersection = new HashSet<char>(set1);
+        intersection.IntersectWith(set2);
+
+        HashSet<char> union = new HashSet<char>(set1);
+        union.UnionWith(set2);
+
+        return (double)intersection.Count / union.Count;
+    }
+
+
+    public static double JaroDistance(this string s1, string s2)
+    {
+        int len1 = s1.Length;
+        int len2 = s2.Length;
+
+        if (len1 == 0) return len2 == 0 ? 1.0 : 0.0;
+
+        int matchDistance = Math.Max(len1, len2) / 2 - 1;
+
+        bool[] s1Matches = new bool[len1];
+        bool[] s2Matches = new bool[len2];
+
+        int matches = 0;
+        int transpositions = 0;
+
+        for (int i = 0; i < len1; i++)
+        {
+            int start = Math.Max(0, i - matchDistance);
+            int end = Math.Min(i + matchDistance + 1, len2);
+
+            for (int j = start; j < end; j++)
+            {
+                if (s2Matches[j]) continue;
+                if (s1[i] != s2[j]) continue;
+                s1Matches[i] = true;
+                s2Matches[j] = true;
+                matches++;
+                break;
+            }
+        }
+
+        if (matches == 0) return 0.0;
+
+        int k = 0;
+        for (int i = 0; i < len1; i++)
+        {
+            if (!s1Matches[i]) continue;
+            while (!s2Matches[k]) k++;
+            if (s1[i] != s2[k]) transpositions++;
+            k++;
+        }
+
+        return ((matches / (double)len1) + (matches / (double)len2) + ((matches - transpositions / 2.0) / matches)) / 3.0;
+    }
+
+    public static double JaroWinklerDistance(this string s1, string s2)
+    {
+        double jaroDist = JaroDistance(s1, s2);
+
+        int prefixLength = 0;
+        for (int i = 0; i < Math.Min(s1.Length, s2.Length); i++)
+        {
+            if (s1[i] == s2[i])
+                prefixLength++;
+            else
+                break;
+        }
+
+        prefixLength = Math.Min(4, prefixLength);
+
+        return jaroDist + (prefixLength * 0.1 * (1 - jaroDist));
+    }
 }
