73dd42f93de8cdb547088440391c2768ad9972ca
Raymond Lei(雷菩宇); 䨻-Ficus religiosa-Cousin <ray810815@gmail.com>
2025-01-04 14:39:53 -0500

    Create proxy.sol
    contract Proxy {
        address public implementation;
        
        function upgrade(address newImplementation) external {
            implementation = newImplementation;
        }
        
        fallback() external {
            address impl = implementation;
            assembly {
                calldatacopy(0, 0, calldatasize())
                let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
                returndatacopy(0, 0, returndatasize())
                switch result
                case 0 { revert(0, returndatasize()) }
                default { return(0, returndatasize()) }
            }
        }
    }

---
+++ Solidity/DesignPatterns/proxy.sol
@@ -0,0 +1,19 @@
+contract Proxy {
+    address public implementation;
+    
+    function upgrade(address newImplementation) external {
+        implementation = newImplementation;
+    }
+    
+    fallback() external {
+        address impl = implementation;
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+}
